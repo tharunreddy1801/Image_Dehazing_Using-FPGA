a1a5bea2ca7792268812b5be58a051d75b1c4327
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-07-18 23:07:50 +0530

    Update Edge_Detection_Top.v

--- Verilog/srcs/TE_Instantiations/Edge_Detection_Top.v
+++ Verilog/srcs/TE_Instantiations/Edge_Detection_Top.v
@@ -1,10 +1,9 @@
 module ED_Top(
-    input        input_is_valid,
     input [23:0] output_pixel_1,
     input [23:0] output_pixel_2,
     input [23:0] output_pixel_3,
     input [23:0] output_pixel_4,
-    input [23:0] output_pixel_5,
+
     input [23:0] output_pixel_6,
     input [23:0] output_pixel_7,
     input [23:0] output_pixel_8,
@@ -13,12 +12,12 @@ module ED_Top(
     output [1:0] ED1_out,ED2_out,ED3_out
 );
     
-    ED red_edges_detection(
+    ED Red_Edges_Detection(
         .in1(output_pixel_1[23:16]),
         .in2(output_pixel_2[23:16]),
         .in3(output_pixel_3[23:16]),
         .in4(output_pixel_4[23:16]),
-        .in5(output_pixel_5[23:16]),
+
         .in6(output_pixel_6[23:16]),
         .in7(output_pixel_7[23:16]),
         .in8(output_pixel_8[23:16]),
@@ -27,12 +26,12 @@ module ED_Top(
         .ED_out(ED1_out)
     );
         
-    ED green_edges_detection(
+    ED Green_Edges_Detection(
         .in1(output_pixel_1[15:8]),
         .in2(output_pixel_2[15:8]),
         .in3(output_pixel_3[15:8]),
         .in4(output_pixel_4[15:8]),
-        .in5(output_pixel_5[15:8]),
+
         .in6(output_pixel_6[15:8]),
         .in7(output_pixel_7[15:8]),
         .in8(output_pixel_8[15:8]),
@@ -41,12 +40,12 @@ module ED_Top(
         .ED_out(ED2_out)
     );
         
-    ED blue_edges_detection(
+    ED Blue_Edges_Detection(
         .in1(output_pixel_1[7:0]),
         .in2(output_pixel_2[7:0]),
         .in3(output_pixel_3[7:0]),
         .in4(output_pixel_4[7:0]),
-        .in5(output_pixel_5[7:0]),
+
         .in6(output_pixel_6[7:0]),
         .in7(output_pixel_7[7:0]),
         .in8(output_pixel_8[7:0]),
