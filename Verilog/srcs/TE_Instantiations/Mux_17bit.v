1aab189d476f6c3e5a02c2a8ab41c78a72feb088
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-07-18 18:25:11 +0530

    Update Mux_17bit.v

--- Verilog/srcs/TE_Instantiations/Mux_17bit.v
+++ Verilog/srcs/TE_Instantiations/Mux_17bit.v
@@ -1,14 +1,14 @@
 //stage 7 multiplexer
-module mux_17bit(
-    input [15:0] m1, m2, m3,
+module Stage_7_Mux(
+    input [15:0] a, b, c,
     input [1:0]  sel,
         
-    output [15:0] p
+    output [15:0] out
 );
     
-    assign p = (sel == 2'b00) ? m1 : 
-               (sel == 2'b01) ? m2 : 
-               (sel == 2'b10) ? m3 :
-               0;
+    assign out = (sel == 2'b00) ? a : 
+                 (sel == 2'b01) ? b : 
+                 (sel == 2'b10) ? c :
+                 0;
     
 endmodule
