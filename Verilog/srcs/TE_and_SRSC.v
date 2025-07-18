12bbefddc4b0d70f9d35cd050c6acd9ce3abb0ef
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-07-18 18:44:15 +0530

    Update TE_and_SRSC.v

--- Verilog/srcs/TE_and_SRSC.v
+++ Verilog/srcs/TE_and_SRSC.v
@@ -1,6 +1,7 @@
+// Transmission Estimation, Scene Recovery and Saturation Correction Module
 module TE_and_SRSC(
-    input       clk,
-    input       rst,
+    input        clk,
+    input        rst,
     
     input        input_is_valid,
     input [23:0] in1,
@@ -43,7 +44,7 @@ module TE_and_SRSC(
     // Multiplier outputs
     wire [15:0] prod0, prod1, prod2;
     
-    // Transmission before subtracting from 1
+    // Transmission before subtracting it from 1
     wire [15:0] pre_transmission;
     
     //==========================================================================
@@ -60,7 +61,7 @@ module TE_and_SRSC(
     // WIRES AND PIPELINE REGISTERS - STAGE 5
     //==========================================================================
     
-    wire [1:0] final_edge = (ed1_reg | ed2_reg | ed3_reg);
+    wire [1:0]  final_edge = (ed1_reg | ed2_reg | ed3_reg);
     
     // Pipeline Registers for stage 5
     reg [1:0]   final_edge_reg_1;
@@ -85,19 +86,19 @@ module TE_and_SRSC(
     wire [15:0] subtract_out;
     
     // Compute (Ic - Ac)
-    wire [7:0] IR_minus_AR, IG_minus_AG, IB_minus_AB;
-    wire       add_or_sub_R, add_or_sub_G, add_or_sub_B;
+    wire [7:0]  IR_minus_AR, IG_minus_AG, IB_minus_AB;
+    wire        add_or_sub_R, add_or_sub_G, add_or_sub_B;
         
     // Inverted transmission value
     wire [15:0] inverse_transmission;
     
-    // Pipeline the center pixel for SRSC
-    reg [23:0] I_0, I_1, I_2, I_3;
+    // Pipeline the Center Pixel for SRSC
+    reg [23:0]  I_0, I_1, I_2;
     
     // Pipeline Atmospheric Light for SRSC
-    reg [7:0]  A_R0, A_G0, A_B0;
-    reg [7:0]  A_R1, A_G1, A_B1;
-    reg [7:0]  A_R2, A_G2, A_B2;
+    reg [7:0]   A_R0, A_G0, A_B0;
+    reg [7:0]   A_R1, A_G1, A_B1;
+    reg [7:0]   A_R2, A_G2, A_B2;
     
     // Pipeline Registers for stage 7
     reg [7:0]   A_R_reg, A_G_reg, A_B_reg;
@@ -110,36 +111,38 @@ module TE_and_SRSC(
     // WIRES AND PIPELINE REGISTERS - STAGE 8
     //==========================================================================
     
-    reg [7:0] A_R_reg1, A_G_reg1, A_B_reg1;
-    reg       add_or_sub_R_reg1, add_or_sub_G_reg1, add_or_sub_B_reg1;
-    reg       stage_8_valid;
+    reg [7:0]   A_R_reg1, A_G_reg1, A_B_reg1;
+    reg         add_or_sub_R_reg1, add_or_sub_G_reg1, add_or_sub_B_reg1;
+    reg         stage_8_valid;
 
     // Compute (Ic-Ac)*(1/T)
-    wire [7:0] Diff_R_times_T, Diff_G_times_T, Diff_B_times_T;
+    wire [7:0]  Diff_R_times_T, Diff_G_times_T, Diff_B_times_T;
     
     //==========================================================================
     // WIRES AND PIPELINE REGISTERS - STAGE 9
     //==========================================================================
     
-    reg [7:0] A_R_reg2, A_G_reg2, A_B_reg2;
-    reg       add_or_sub_R_reg2, add_or_sub_G_reg2, add_or_sub_B_reg2;
-    reg [7:0] Mult_Red_Reg, Mult_Green_Reg, Mult_Blue_Reg;
-    reg       stage_9_valid;
+    reg [7:0]   A_R_reg2, A_G_reg2, A_B_reg2;
+    reg         add_or_sub_R_reg2, add_or_sub_G_reg2, add_or_sub_B_reg2;
+    reg [7:0]   Mult_Red_Reg, Mult_Green_Reg, Mult_Blue_Reg;
+    reg         stage_9_valid;
     
     // Compute Ac +/- (|I-A|/t)
-    wire [7:0] Sum_Red, Sum_Green, Sum_Blue;
+    wire [7:0]  Sum_Red, Sum_Green, Sum_Blue;
     
     //==========================================================================
     // WIRES AND PIPELINE REGISTERS - STAGE 10
     //==========================================================================
     
-    reg [7:0] J_R_reg, J_G_reg, J_B_reg;
-    reg       stage_10_valid;
-    
+    reg [7:0]   J_R_reg, J_G_reg, J_B_reg;
+    reg         stage_10_valid;
+
+    // Outputs of Look-Up Tables for Saturation Corection
     wire [15:0] J_R_Corrected, J_G_Corrected, J_B_Corrected;
     wire [15:0] A_R_Corrected, A_G_Corrected, A_B_Corrected;
-    
-    wire [7:0] SC_R, SC_G, SC_B;
+
+    // Product of corrected Ac and Jc
+    wire [7:0]  SC_R, SC_G, SC_B;
     
     //==========================================================================
     // UPDATING PIPELINE REGISTERS
@@ -244,6 +247,9 @@ module TE_and_SRSC(
     assign I_R = I_2[23:16];
     assign I_G = I_2[15:8];
     assign I_B = I_2[7:0];
+
+    // Further pipeline Center Pixel for the Adder module
+    reg [7:0] I_R1, I_G1, I_B1, I_R2, I_G2, I_B2, I_R3, I_G3, I_B3;
     
     // Update Stage 7 Pipeline Registers
     always @(posedge clk) begin
@@ -254,6 +260,10 @@ module TE_and_SRSC(
             A_G_reg <= 0;
             A_B_reg <= 0;
             
+            I_R1 <= 0;
+            I_G1 <= 0;
+            I_B1 <= 0;
+            
             add_or_sub_R_reg <= 0;
             add_or_sub_G_reg <= 0;
             add_or_sub_B_reg <= 0;
@@ -262,7 +272,6 @@ module TE_and_SRSC(
             IG_minus_AG_reg <= 0;
             IB_minus_AB_reg <= 0;
             
-            inverse_transmission_reg <= 0;
             
             stage_7_valid <= 0;
         end
@@ -273,6 +282,10 @@ module TE_and_SRSC(
             A_G_reg <= A_G2;
             A_B_reg <= A_B2;
             
+            I_R1 <= I_R;
+            I_G1 <= I_G;
+            I_B1 <= I_B;
+            
             add_or_sub_R_reg <= add_or_sub_R;
             add_or_sub_G_reg <= add_or_sub_G;
             add_or_sub_B_reg <= add_or_sub_B;
@@ -281,7 +294,7 @@ module TE_and_SRSC(
             IG_minus_AG_reg <= IG_minus_AG;
             IB_minus_AB_reg <= IB_minus_AB;
             
-            inverse_transmission_reg <= inverse_transmission;
+
                 
             stage_7_valid <= stage_6_valid;
         end
@@ -293,10 +306,16 @@ module TE_and_SRSC(
             A_R_reg1 <= 0;
             A_G_reg1 <= 0;
             A_B_reg1 <= 0;
+            
+            I_R2 <= 0;
+            I_G2 <= 0;
+            I_B2 <= 0;
 
             add_or_sub_R_reg1 <= 0;
             add_or_sub_G_reg1 <= 0;
             add_or_sub_B_reg1 <= 0;
+            
+            inverse_transmission_reg <= 0;
             
             stage_8_valid <= 0;
         end
@@ -304,10 +323,16 @@ module TE_and_SRSC(
             A_R_reg1 <= A_R_reg;
             A_G_reg1 <= A_G_reg;
             A_B_reg1 <= A_B_reg;
+            
+            I_R2 <= I_R1;
+            I_G2 <= I_G1;
+            I_B2 <= I_B1;
 
             add_or_sub_R_reg1 <= add_or_sub_R_reg;
             add_or_sub_G_reg1 <= add_or_sub_G_reg;
             add_or_sub_B_reg1 <= add_or_sub_B_reg;
+            
+            inverse_transmission_reg <= inverse_transmission;
 
             stage_8_valid <= stage_7_valid;
         end
@@ -319,6 +344,10 @@ module TE_and_SRSC(
             A_R_reg2 <= 0;
             A_G_reg2 <= 0;
             A_B_reg2 <= 0;
+            
+            I_R3 <= 0;
+            I_G3 <= 0;
+            I_B3 <= 0;
             
             add_or_sub_R_reg2 <= 0;
             add_or_sub_G_reg2 <= 0;
@@ -334,6 +363,10 @@ module TE_and_SRSC(
             A_R_reg2 <= A_R_reg1;
             A_G_reg2 <= A_G_reg1;
             A_B_reg2 <= A_B_reg1;
+            
+            I_R3 <= I_R2;
+            I_G3 <= I_G2;
+            I_B3 <= I_B2;
             
             add_or_sub_R_reg2 <= add_or_sub_R_reg1;
             add_or_sub_G_reg2 <= add_or_sub_G_reg1;
@@ -389,7 +422,7 @@ module TE_and_SRSC(
     // P0 BLOCKS FOR MEAN FILTERING
     //==========================================================================
     
-    block_P0 P0_Red(
+    Block_P0 P0_Red(
         .in1(in1[23:16]), .in2(in2[23:16]), .in3(in3[23:16]),
         .in4(in4[23:16]), .in5(in5[23:16]), .in6(in6[23:16]),
         .in7(in7[23:16]), .in8(in8[23:16]), .in9(in9[23:16]),
@@ -397,7 +430,7 @@ module TE_and_SRSC(
         .p0_result(p0_red_out)
     );
     
-    block_P0 P0_Green(
+    Block_P0 P0_Green(
         .in1(in1[15:8]), .in2(in2[15:8]), .in3(in3[15:8]),
         .in4(in4[15:8]), .in5(in5[15:8]), .in6(in6[15:8]),
         .in7(in7[15:8]), .in8(in8[15:8]), .in9(in9[15:8]),
@@ -405,7 +438,7 @@ module TE_and_SRSC(
         .p0_result(p0_green_out)
     );
     
-    block_P0 P0_Blue(
+    Block_P0 P0_Blue(
         .in1(in1[7:0]), .in2(in2[7:0]), .in3(in3[7:0]),
         .in4(in4[7:0]), .in5(in5[7:0]), .in6(in6[7:0]),
         .in7(in7[7:0]), .in8(in8[7:0]), .in9(in9[7:0]),
@@ -417,7 +450,7 @@ module TE_and_SRSC(
     // P1 BLOCKS FOR EDGE PRESERVING
     //==========================================================================
     
-    block_P1 P1_Red(
+    Block_P1 P1_Red(
         .in1(in1[23:16]), .in2(in2[23:16]), .in3(in3[23:16]),
         .in4(in4[23:16]), .in5(in5[23:16]), .in6(in6[23:16]),
         .in7(in7[23:16]), .in8(in8[23:16]), .in9(in9[23:16]),
@@ -425,7 +458,7 @@ module TE_and_SRSC(
         .p1_result(p1_red_out)
     );
     
-    block_P1 P1_Green(
+    Block_P1 P1_Green(
         .in1(in1[15:8]), .in2(in2[15:8]), .in3(in3[15:8]),
         .in4(in4[15:8]), .in5(in5[15:8]), .in6(in6[15:8]),
         .in7(in7[15:8]), .in8(in8[15:8]), .in9(in9[15:8]),
@@ -433,7 +466,7 @@ module TE_and_SRSC(
         .p1_result(p1_green_out)
     );
     
-    block_P1 P1_Blue(
+    Block_P1 P1_Blue(
         .in1(in1[7:0]), .in2(in2[7:0]), .in3(in3[7:0]),
         .in4(in4[7:0]), .in5(in5[7:0]), .in6(in6[7:0]),
         .in7(in7[7:0]), .in8(in8[7:0]), .in9(in9[7:0]),
@@ -445,7 +478,7 @@ module TE_and_SRSC(
     // P2 BLOCKS FOR EDGE PRESERVING
     //==========================================================================
     
-    block_P2 P2_Red(
+    Block_P2 P2_Red(
         .in1(in1[23:16]), .in2(in2[23:16]), .in3(in3[23:16]),
         .in4(in4[23:16]), .in5(in5[23:16]), .in6(in6[23:16]),
         .in7(in7[23:16]), .in8(in8[23:16]), .in9(in9[23:16]),
@@ -453,7 +486,7 @@ module TE_and_SRSC(
         .p2_result(p2_red_out)
     );
     
-    block_P2 P2_Green(
+    Block_P2 P2_Green(
         .in1(in1[15:8]), .in2(in2[15:8]), .in3(in3[15:8]),
         .in4(in4[15:8]), .in5(in5[15:8]), .in6(in6[15:8]),
         .in7(in7[15:8]), .in8(in8[15:8]), .in9(in9[15:8]),
@@ -461,7 +494,7 @@ module TE_and_SRSC(
         .p2_result(p2_green_out)
     );
     
-    block_P2 P2_Blue(
+    Block_P2 P2_Blue(
         .in1(in1[7:0]), .in2(in2[7:0]), .in3(in3[7:0]),
         .in4(in4[7:0]), .in5(in5[7:0]), .in6(in6[7:0]),
         .in7(in7[7:0]), .in8(in8[7:0]), .in9(in9[7:0]),
@@ -535,7 +568,7 @@ module TE_and_SRSC(
         .out(minimum_p2)
     );
     
-    // Min atmospheric light muxes
+    // Minimum atmospheric light muxes
     Mux_2 InvA_0_Mux(
         .a(inv_ar1_1),
         .b(inv_ag1_1),
@@ -571,43 +604,43 @@ module TE_and_SRSC(
     //==========================================================================
     
     Multiplier multiply_P0(
-        .a(inv_mux0_reg),
-        .b(P0_reg),
+        .Ac_Inv(inv_mux0_reg),
+        .Pc(P0_reg),
         
         .product(prod0)
     );
     
     Multiplier multiply_P1(
-        .a(inv_mux1_reg),
-        .b(P1_reg),
+        .Ac_Inv(inv_mux1_reg),
+        .Pc(P1_reg),
         
         .product(prod1)
     );
     
     Multiplier multiply_P2(
-        .a(inv_mux2_reg),
-        .b(P2_reg),
+        .Ac_Inv(inv_mux2_reg),
+        .Pc(P2_reg),
         
         .product(prod2)
     );
     
-    // 17 bit multiplexer
-    mux_17bit Stage_7_Mux(
-        .m1(prod0), .m2(prod1), .m3(prod2),
+    // Stage 7 16-bit multiplexer
+    Stage_7_Mux Stage_7_Mux(
+        .a(prod0), .b(prod1), .c(prod2),
         
         .sel(final_edge_reg_2),
         
-        .p(pre_transmission)
+        .out(pre_transmission)
     );
 
-    // Subtractor instantiation
+    // Subtractor to compute 1 - pre_transmission
     Subtractor Sub(
-        .a(pre_transmission),
+        .in(pre_transmission),
         .diff(subtract_out)
     );
     
     //==========================================================================
-    // SUBTRACTOR MODULES TO COMPUTE (Ic - Ac)
+    // SUBTRACTOR MODULES TO COMPUTE (|Ic - Ac|)
     //==========================================================================
     
     Subtractor_SRSC Sub_Red(
@@ -615,6 +648,7 @@ module TE_and_SRSC(
         .Ac(A_R2),
     
         .diff(IR_minus_AR),
+        
         .add_or_sub(add_or_sub_R)
     );
     
@@ -623,6 +657,7 @@ module TE_and_SRSC(
         .Ac(A_G2),
     
         .diff(IG_minus_AG),
+        
         .add_or_sub(add_or_sub_G)
     );
 
@@ -631,6 +666,7 @@ module TE_and_SRSC(
         .Ac(A_B2),
     
         .diff(IB_minus_AB),
+        
         .add_or_sub(add_or_sub_B)
     );
     
@@ -638,7 +674,7 @@ module TE_and_SRSC(
     // TRANSMISSION RECIPROCAL LOOKUP TABLE
     //==========================================================================
     
-    // LUT to output the inverse of transmission values(Q0.16) in Q2.14 format
+    // LUT to output the reciprocal of transmission values (Q0.16) in Q2.14 format
     Trans_LUT Transmission_Reciprocal_LUT(
         .x(transmission),
         
@@ -646,7 +682,7 @@ module TE_and_SRSC(
     );
 
     //==========================================================================
-    // MULTIPLIER MODULES TO COMPUTE (Ic-Ac)*(1/T)
+    // MULTIPLIER MODULES TO COMPUTE (Ic-Ac)*(1/t)
     //==========================================================================
     
     Multiplier_SRSC Mult_Red(
@@ -676,6 +712,7 @@ module TE_and_SRSC(
     
     Adder_SRSC Add_Red(
         .Ac(A_R_reg2),
+        .Ic(I_R3),
         .Multiplier_out(Mult_Red_Reg),
         
         .add_or_sub(add_or_sub_R_reg2),
@@ -685,6 +722,7 @@ module TE_and_SRSC(
         
     Adder_SRSC Add_Green(
         .Ac(A_G_reg2),
+        .Ic(I_G3),
         .Multiplier_out(Mult_Green_Reg),
         
         .add_or_sub(add_or_sub_G_reg2),
@@ -694,6 +732,7 @@ module TE_and_SRSC(
         
     Adder_SRSC Add_Blue(
         .Ac(A_B_reg2),
+        .Ic(I_B3),
         .Multiplier_out(Mult_Blue_Reg),
         
         .add_or_sub(add_or_sub_B_reg2),
@@ -702,41 +741,41 @@ module TE_and_SRSC(
     );
     
     //==========================================================================
-    // LOOK-UP TABLES TO COMPUTE Ac ^ 0.3 AND J ^ 0.7
+    // LOOK-UP TABLES TO COMPUTE Ac ^ β AND J ^ (1 - β) (β = 0.2/0.3)
     //==========================================================================
     
-    LUT_03 A_R_Correction(
+    LUT_02 A_R_Correction(
         .x(A_R_reg2),
         .y_q8_8(A_R_Corrected)
     );
     
-    LUT_03 A_G_Correction(
+    LUT_02 A_G_Correction(
         .x(A_G_reg2),
         .y_q8_8(A_G_Corrected)
     );
     
-    LUT_03 A_B_Correction(
+    LUT_02 A_B_Correction(
         .x(A_B_reg2),
         .y_q8_8(A_B_Corrected)
     );
     
-    LUT_07 J_R_Correction(
+    LUT_08 J_R_Correction(
         .x(Sum_Red),
         .y_q8_8(J_R_Corrected)
     );
     
-    LUT_07 J_G_Correction(
+    LUT_08 J_G_Correction(
         .x(Sum_Green),
         .y_q8_8(J_G_Corrected)
     );
     
-    LUT_07 J_B_Correction(
+    LUT_08 J_B_Correction(
         .x(Sum_Blue),
         .y_q8_8(J_B_Corrected)
     );
     
     //==========================================================================
-    // MULTIPLIER MODULES TO COMPUTE Ac^? × Jc^(1-?)
+    // MULTIPLIER MODULES TO COMPUTE Ac^β × Jc^(1-β)
     //==========================================================================
     Saturation_Correction_Multiplier Saturation_Correction_Red(
         .x1(A_R_Corrected), .x2(J_R_Corrected),
