4888c6d9ec43629353c77bd5d696e0bc6e7c41f9
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-07-18 18:29:11 +0530

    Update Adder_SRSC.v

--- Verilog/srcs/SRSC_Instantiations/Adder_SRSC.v
+++ Verilog/srcs/SRSC_Instantiations/Adder_SRSC.v
@@ -1,5 +1,6 @@
 module Adder_SRSC(
     input [7:0]  Ac,
+    input [7:0]  Ic,
     input [7:0]  Multiplier_out,
     
     input        add_or_sub,
@@ -7,7 +8,7 @@ module Adder_SRSC(
     output [7:0] out
 );
     
-    assign out = add_or_sub ? ((Ac+ Multiplier_out > 8'd255) ? 8'd255 : (Ac + Multiplier_out)) :
-                              ((Ac > Multiplier_out) ? (Ac - Multiplier_out) : 8'd0);
-    
+    assign out = add_or_sub ? (Ic > Ac) ? (Ac - Multiplier_out) : (Ac + Multiplier_out) :
+                                          (Ac > Multiplier_out) ? (Ac - Multiplier_out) : 8'd0;
+
 endmodule
