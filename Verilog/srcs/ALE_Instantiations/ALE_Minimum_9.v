94a808f4203a2892f47cdc61beb8a775081ddc2f
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-07-18 18:44:55 +0530

    Update ALE_Minimum_9.v

--- Verilog/srcs/ALE_Instantiations/ALE_Minimum_9.v
+++ Verilog/srcs/ALE_Instantiations/ALE_Minimum_9.v
@@ -1,14 +1,14 @@
 // Find the minimum of each color channel inputs
 module ALE_Minimum_9(
-    input  [7:0]   input_pixel_1,
-    input  [7:0]   input_pixel_2,
-    input  [7:0]   input_pixel_3,
-    input  [7:0]   input_pixel_4,
-    input  [7:0]   input_pixel_5,
-    input  [7:0]   input_pixel_6,
-    input  [7:0]   input_pixel_7,
-    input  [7:0]   input_pixel_8,
-    input  [7:0]   input_pixel_9, // 3x3 R/G/B color channel input
+    input [7:0]  input_pixel_1,
+    input [7:0]  input_pixel_2,
+    input [7:0]  input_pixel_3,
+    input [7:0]  input_pixel_4,
+    input [7:0]  input_pixel_5,
+    input [7:0]  input_pixel_6,
+    input [7:0]  input_pixel_7,
+    input [7:0]  input_pixel_8,
+    input [7:0]  input_pixel_9, // 3x3 R/G/B color channel input
     
     output [7:0] minimum_pixel
 );
