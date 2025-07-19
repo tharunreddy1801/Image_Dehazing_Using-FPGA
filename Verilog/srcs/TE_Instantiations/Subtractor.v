8b0ffef56a68c4adabfe17166de69e194ec1e624
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-07-19 19:44:31 +0530

    Update Subtractor.v

--- Verilog/srcs/TE_Instantiations/Subtractor.v
+++ Verilog/srcs/TE_Instantiations/Subtractor.v
@@ -5,7 +5,7 @@ module Subtractor(
 );
     
     parameter [16:0] ONE = 17'd65536; // 1.0 in Q0.17 format
-    parameter [15:0] T0  = 16'd16384; // Lower bound for transmission is 0.25 in Q0.16 format
+    parameter [15:0] T0  = 16'd18022; // Lower bound for transmission - 0.275 in Q0.16 format
     
     assign diff = ((ONE - in) < T0) ?  T0 : ONE - in;
     
