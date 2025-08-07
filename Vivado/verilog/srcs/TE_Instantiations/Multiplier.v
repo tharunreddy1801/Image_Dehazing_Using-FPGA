46bea40369b505f0bdf7793408653e0147be6edd
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-07 15:40:06 +0530

    Update Multiplier.v

--- Vivado/verilog/srcs/TE_Instantiations/Multiplier.v
+++ Vivado/verilog/srcs/TE_Instantiations/Multiplier.v
@@ -1,27 +1,39 @@
-module Multiplier(
+module Multiplier (
+    input clk,
+    input rst,
     input  [15:0] Ac_Inv, // Inverted Atmospheric Light value in Q0.16 format
     input  [7:0]  Pc,     // Edge Detection Filter result
     output [15:0] product // OMEGA * min(Pc / Ac) ; c ∈ {R, G, B} in Q0.16 format
 );
 
-    // Constants in Q0.16 format
-    parameter [15:0] OMEGA      = 16'd61440;   // 0.9375 in Q0.16
-    parameter [15:0] MAX_OUTPUT = 16'd47415;   // (1 - 0.275) = 0.725 in Q0.16
-
-    // Unscaled result is in Q8.16 format
-    wire [23:0] unscaled_product = Ac_Inv * Pc;
-
-    // Scale the result with OMEGA (Q0.16)
-    // Scaled result is in Q8.32 format
-    wire [39:0] scaled_product = unscaled_product * OMEGA;
-
-    // Trim the product down to Q0.16 format
-    wire [15:0] result = scaled_product[31:16];
-
-    // Check if the unscaled product is greater than 1 to prevent roll around due to negative result in the Subtractor circuit
+    parameter [15:0] MAX_OUTPUT = 16'd47415;   // 0.725 in Q0.16
+    
+    wire [23:0] unscaled_product;
+    
+    reg [15:0] Ac_Inv_P;
+    reg [7:0]  Pc_P;
+    
+    // Pipeline inputs to reduce fan-out
+    always @(posedge clk) begin
+        if(rst) begin
+            Ac_Inv_P <= 0;
+            Pc_P <= 0;
+        end
+        else begin
+            Ac_Inv_P <= Ac_Inv;
+            Pc_P <= Pc;
+        end
+    end
+    
+    assign unscaled_product = Ac_Inv_P * Pc_P;
+    
+    // Combinational logic for OMEGA scaling
+    wire [27:0] pre_scaled_product = (unscaled_product << 4) - unscaled_product;
+    wire [23:0] scaled_product = pre_scaled_product >> 4;
+    wire [15:0] result = scaled_product[15:0];
+    
+    // Overflow detection and output
     wire is_gt_one = (unscaled_product[23:16] != 0);
-
-    // Clamp output to 0.75 if overflow occurred
     assign product = is_gt_one ? MAX_OUTPUT : result;
-
+    
 endmodule
