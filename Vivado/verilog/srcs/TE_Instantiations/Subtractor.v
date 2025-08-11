921274ec7f4a5829fd1512f5def920f7d29c2f7d
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-11 22:21:36 +0530

    Update Subtractor.v

--- Vivado/verilog/srcs/TE_Instantiations/Subtractor.v
+++ Vivado/verilog/srcs/TE_Instantiations/Subtractor.v
@@ -1,12 +1,14 @@
-module Subtractor(
-    input  [15:0] in,  // OMEGA * min(Pc / Ac) ; c ∈ {R, G, B}
+// Compute T(x) = 1 - ω * min(Pc / Ac)
+module Subtractor (
+    input  [15:0] in, // ω * min(Pc / Ac) : c ∈ {R, G, B}
     
-    output [15:0] diff // Final Transmission value
+    output [15:0] out // Transmission value in Q0.16
 );
     
-    parameter [16:0] ONE = 17'd65536; // 1.0 in Q0.17 format
-    parameter [15:0] T0  = 16'd18022; // Lower bound for transmission - 0.275 in Q0.16 format
+    parameter [15:0] ONE = 16'd65535; // 0.999.... = 1.0 in Q0.16 format
+    parameter [15:0] T0  = 16'd21299; // Lower bound for transmission = 0.325 in Q0.16 format
+    parameter [15:0] MAX_T  = 16'd44237; // ONE - T0 = 0.675 in Q0.16 format
     
-    assign diff = ((ONE - in) < T0) ?  T0 : ONE - in;
+    assign out = (in > MAX_T) ?  T0 : ONE - in;
     
 endmodule
