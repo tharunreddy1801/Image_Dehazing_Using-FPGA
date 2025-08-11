d93dc53ca410484148c4a982fcf64d906cc4743c
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-11 22:24:49 +0530

    Update WindowGeneratorTop.v

--- Vivado/verilog/srcs/Window_Generation/WindowGeneratorTop.v
+++ Vivado/verilog/srcs/Window_Generation/WindowGeneratorTop.v
@@ -1,4 +1,5 @@
-module WindowGeneratorTop(
+// Top module for the Window Generator which takes the pixel stream as input and outputs 3x3 windows of RGB Pixels
+module WindowGeneratorTop (
     input         clk,
     input         rst,
     
@@ -20,7 +21,7 @@ module WindowGeneratorTop(
     wire [23:0] dlb_out_1,dlb_out_2,dlb_out_3;
     wire        dlb_out_valid;
     
-    Double_LineBuffer Double_LineBuffer(
+    Double_LineBuffer Double_LineBuffer (
         .clk(clk),
         .rst(rst),
         
@@ -33,7 +34,7 @@ module WindowGeneratorTop(
         .output_is_valid(dlb_out_valid)
     );
     
-    WindowGenerator WindowGenerator(
+    WindowGenerator WindowGenerator (
         .clk(clk),
         .rst(rst),
             
