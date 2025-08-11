d5518ceb27151bdad567587751686179b97e4ab3
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-11 22:08:20 +0530

    Update Subtractor_SRSC.v

--- Vivado/verilog/srcs/SRSC_Instantiations/Subtractor_SRSC.v
+++ Vivado/verilog/srcs/SRSC_Instantiations/Subtractor_SRSC.v
@@ -1,13 +1,15 @@
-module Subtractor_SRSC(
+module Subtractor_SRSC (
     input [7:0]  Ic,
     input [7:0]  Ac, 
     
-    output [7:0] diff,
-    output       add_or_sub
+    output [7:0] diff,      // |Ic - Ac|
+    output       add_or_sub // Add with or subtract from Atmospheric Light in the Adder module
 );
-
+    
+    parameter ADD = 1, SUB = 0;
+    
     assign diff = (Ic > Ac) ? (Ic - Ac) : (Ac - Ic);
     
-    assign add_or_sub = (Ic > Ac) ? 1 : 0;
+    assign add_or_sub = (Ic > Ac) ? ADD : SUB;
     
 endmodule
