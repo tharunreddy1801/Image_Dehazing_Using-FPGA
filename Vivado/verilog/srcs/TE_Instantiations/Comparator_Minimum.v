ac991667e5229aacc2400800cb99d682516768c0
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-11 22:13:33 +0530

    Update Comparator_Minimum.v

--- Vivado/verilog/srcs/TE_Instantiations/Comparator_Minimum.v
+++ Vivado/verilog/srcs/TE_Instantiations/Comparator_Minimum.v
@@ -1,10 +1,13 @@
-module Comparator_Minimum(
-    input [7:0]  red, green, blue,
+// Comparator module which outputs the minimum value of the inputs
+module Comparator_Minimum (
+    input  [7:0] red, green, blue,
     output [1:0] min_val
 );
-
-    assign min_val = (red <= green && red <= blue) ? 2'b00 :
-                     (green <= red && green <= blue) ? 2'b01 :
-                     2'b10;
+    
+    parameter RED = 2'b00, GREEN = 2'b01, BLUE = 2'b10;
+    
+    assign min_val = (red <= green && red <= blue)   ? RED : 
+                     (green <= red && green <= blue) ? GREEN : 
+                                                       BLUE;
 
 endmodule
