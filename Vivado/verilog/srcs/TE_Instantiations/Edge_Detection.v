53a6bad7fa56c629195a3a868e95fd5eae9bdc53
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-11 22:14:08 +0530

    Update Edge_Detection.v

--- Vivado/verilog/srcs/TE_Instantiations/Edge_Detection.v
+++ Vivado/verilog/srcs/TE_Instantiations/Edge_Detection.v
@@ -1,28 +1,34 @@
-module ED(
-    input  wire [7:0] in1,
-    input  wire [7:0] in2,
-    input  wire [7:0] in3,
-    input  wire [7:0] in4,
 
-    input  wire [7:0] in6,
-    input  wire [7:0] in7,
-    input  wire [7:0] in8,
-    input  wire [7:0] in9,
+module ED (
+    input  [7:0] input_pixel_1,
+    input  [7:0] input_pixel_2,
+    input  [7:0] input_pixel_3,
+    input  [7:0] input_pixel_4,
+
+    input  [7:0] input_pixel_6,
+    input  [7:0] input_pixel_7,
+    input  [7:0] input_pixel_8,
+    input  [7:0] input_pixel_9,
     
-    output wire [1:0] ED_out
+    output [1:0] ED_out
 );
-    parameter D = 8'b01010000; // Threshold Value = 80
+
+    parameter DIAGONAL_EDGE = 2, 
+              VERTICAL_HORIZONTAL_EDGE = 1, 
+              NO_EDGE = 0;
     
-    wire [7:0] diag1_diff = (in1 > in9) ? (in1 - in9) : (in9 - in1);
-    wire [7:0] diag2_diff = (in3 > in7) ? (in3 - in7) : (in7 - in3);
-    wire [7:0] horizontal_diff = (in4 > in6) ? (in4 - in6) : (in6 - in4);
-    wire [7:0] vert_diff = (in2 > in8) ? (in2 - in8) : (in8 - in2);
+    parameter THRESHOLD = 80; // Threshold Value for Edge Detection
     
-    wire cond1 = (diag1_diff >= D) || (diag2_diff >= D);
-    wire cond2 = (horizontal_diff >= D) || (vert_diff >= D);
+    wire [7:0] diagonal1  = (input_pixel_1 > input_pixel_9) ? (input_pixel_1 - input_pixel_9) : (input_pixel_9 - input_pixel_1);
+    wire [7:0] diagonal2  = (input_pixel_3 > input_pixel_7) ? (input_pixel_3 - input_pixel_7) : (input_pixel_7 - input_pixel_3);
+    wire [7:0] horizontal = (input_pixel_4 > input_pixel_6) ? (input_pixel_4 - input_pixel_6) : (input_pixel_6 - input_pixel_4);
+    wire [7:0] vertical   = (input_pixel_2 > input_pixel_8) ? (input_pixel_2 - input_pixel_8) : (input_pixel_8 - input_pixel_2);
     
-    assign ED_out = cond1 ? 2'b10 : 
-                    cond2 ? 2'b01 : 
-                    2'b00;
+    wire cond1 = (diagonal1 >= THRESHOLD) || (diagonal2 >= THRESHOLD);
+    wire cond2 = (horizontal >= THRESHOLD) || (vertical >= THRESHOLD);
+    
+    assign ED_out = cond1 ? DIAGONAL_EDGE : 
+                    cond2 ? VERTICAL_HORIZONTAL_EDGE : 
+                    NO_EDGE;
 
 endmodule
