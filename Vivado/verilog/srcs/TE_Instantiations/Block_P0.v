c03874253b681cc411e27acec6ba33ea572257de
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-11 22:11:40 +0530

    Update Block_P0.v

--- Vivado/verilog/srcs/TE_Instantiations/Block_P0.v
+++ Vivado/verilog/srcs/TE_Instantiations/Block_P0.v
@@ -1,5 +1,7 @@
-// Mean Filter when no edges are detected
+// Mean Filter applied when no edges are detected
 module Block_P0 (
+    input clk, rst, 
+    
     input  [7:0] in1,
     input  [7:0] in2,
     input  [7:0] in3,
@@ -13,9 +15,33 @@ module Block_P0 (
     output [7:0] p0_result
 );
     
-    wire [11:0] sum;
-    assign sum = in1 + in2 + in3 + in4 + in5 + in6 + in7 + in8 + in9;
+    // Pipeline Registers
+    reg [7:0] in1_P, in2_P, in3_P, 
+              in4_P, in5_P, in6_P, 
+              in7_P, in8_P, in9_P;
     
-    assign p0_result = sum / 9;
+    wire [9:0]  sum1, sum2, sum3;
+    wire [14:0] product;
+    
+    always @(posedge clk) begin
+        if(rst) begin
+            in1_P <= 0; in2_P <= 0; in3_P <= 0;
+            in4_P <= 0; in5_P <= 0; in6_P <= 0;
+            in7_P <= 0; in8_P <= 0; in9_P <= 0;
+        end
+        else begin
+            in1_P <= in1; in2_P <= in2; in3_P <= in3;
+            in4_P <= in4; in5_P <= in5; in6_P <= in6;
+            in7_P <= in7; in8_P <= in8; in9_P <= in9;
+        end
+    end
+
+    assign sum1 = in1_P + in2_P + in3_P;
+    assign sum2 = in4_P + in5_P + in6_P;
+    assign sum3 = in7_P + in8_P + in9_P;
+    
+    assign product = ((sum1 + sum2 + sum3) << 4) - ((sum1 + sum2 + sum3) << 1);
+    // Mutiply by 14 and then divide by 128 -> 14/128 = 1/9 (approx.)
+    assign p0_result = product >> 7;
     
 endmodule
