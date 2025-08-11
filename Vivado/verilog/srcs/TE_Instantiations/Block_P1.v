c26aca0bcefb082654746e951293bccdce976ee4
yennam sai tharun reddy <162603451+tharunreddy1801@users.noreply.github.com>
2025-08-11 22:12:12 +0530

    Update Block_P1.v

--- Vivado/verilog/srcs/TE_Instantiations/Block_P1.v
+++ Vivado/verilog/srcs/TE_Instantiations/Block_P1.v
@@ -1,5 +1,7 @@
-// Edge Preserving Filter for Vertical and Horizontal edges
+// Edge Preserving Filter applied when Vertical and Horizontal edges are detected
 module Block_P1 (
+    input clk, rst, 
+    
     input  [7:0] in1,
     input  [7:0] in2,
     input  [7:0] in3,
@@ -13,9 +15,33 @@ module Block_P1 (
     output [7:0] p1_result
 );
     
-    wire [11:0] sum;
-    assign sum = (in1) + (in2 << 1) + (in3) + (in4 << 1) + (in5 << 2) + (in6 << 1) + (in7) + (in8 << 1) + (in9);
+    // Pipeline Registers
+    reg [7:0]   in1_P, in2_P, in3_P, 
+                in4_P, in5_P, in6_P, 
+                in7_P, in8_P, in9_P;
     
+    wire [10:0] sum1, sum2, sum3;
+    wire [11:0] sum;
+    
+    always @(posedge clk) begin
+        if(rst) begin
+            in1_P <= 0; in2_P <= 0; in3_P <= 0;
+            in4_P <= 0; in5_P <= 0; in6_P <= 0;
+            in7_P <= 0; in8_P <= 0; in9_P <= 0;
+        end
+        else begin
+            in1_P <= in1; in2_P <= in2; in3_P <= in3;
+            in4_P <= in4; in5_P <= in5; in6_P <= in6;
+            in7_P <= in7; in8_P <= in8; in9_P <= in9;
+        end
+    end
+
+    assign sum1 = in1_P + (in2_P << 1) + in3_P;
+    assign sum2 = (in4_P << 1) + (in5_P << 2) + (in6_P << 1);
+    assign sum3 = in7_P + (in8_P << 1) + in9_P;
+
+    assign sum = sum1 + sum2 + sum3;
+
     assign p1_result = sum >> 4;
     
 endmodule
